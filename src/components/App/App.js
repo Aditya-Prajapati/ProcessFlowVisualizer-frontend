85706a7f8d008670e434239ca4aa0e4aeb1198ea
Aditya Prajapati <adityaprajapati28@gmail.com>
2024-06-14 18:23:45 +0530

    Getting started, first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Header, Footer } from "../exports";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import "./Header.css";
+
+const Header = ({  }) => {
+  return (
+    <div className="header text-3xl font-bold underline ">
+      
+    </div>
+  );
+}
+
+export default Header;
